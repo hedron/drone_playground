01aae6a80f01fbf22a7d357a959dfd68b9cdd83b
Jeff Wang <jwang@quantcast.com>
2015-12-20 04:01:24 -0800

    Add fib function and test

--- hi.go
+++ hi.go
@@ -1,7 +1,20 @@
-package hi
+package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("hi")
+    fmt.Println(fib(50))
+}
+
+
+func fib(n int) int {
+    current := 0
+    previous := 1
+    tmp := 0
+    for i := 0; i < n; i++ {
+        tmp = current
+        current += previous
+        previous = tmp
+    }
+    return current
 }
